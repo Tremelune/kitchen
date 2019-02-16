667efdb3700bb8961fad20a5ccc835f186037321
Will <will.benedict@fabfitfun.com>
2019-02-15 21:56:01 -0800

    No need for the ID comparison anymore, as we're not messing with SortedSets.

--- src/main/java/wtf/benedict/kitchen/biz/RemainingShelfLifeComparator.java
+++ src/main/java/wtf/benedict/kitchen/biz/RemainingShelfLifeComparator.java
@@ -6,10 +6,9 @@ class RemainingShelfLifeComparator {
   static final Comparator<Order> INSTANCE = newRemainingShelfLifeComparator();
 
 
+  // Note: If this is used in a SortedSet, it will treat elements with the same remaining shelf life
+  // as the same, and one will disappear unless you also include an ID comparison. Ask me how I know...
   private static Comparator<Order> newRemainingShelfLifeComparator() {
-    // We need to include ID, or sort will consider elements with the same remainingShelfLife to be
-    // identical. TODO NOT ANYMORE
-    return Comparator.comparingLong(Order::calculateRemainingShelfLife)
-        .thenComparing(Order::getId);
+    return Comparator.comparingLong(Order::calculateRemainingShelfLife);
   }
 }
